32884223e72c04925bd50ff2283856cf67a3b355
Eduardo Batista <pro.eduardobatistanogueira@gmail.com>
2025-07-07 22:17:26 -0300

    feat: integrate Firebase authentication and create AuthContext
    - Added Firebase dependency to package.json
    - Implemented AuthContext to manage user authentication state
    - Created Private route component to protect dashboard routes
    - Updated App component to use Private routes for dashboard and new car pages
    - Modified Header component to utilize AuthContext for user state
    - Enhanced Login and Register pages to handle user authentication with Firebase
    - Created DashboardHeader component for navigation and logout functionality
    - Established Firebase connection service for app initialization

---
+++ src/services/firebaseConnection.ts
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+import {getStorage} from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDUlbVaa8fgJCoweHbrE0mKW5YEGDDo-dM",
+  authDomain: "webcarros-6ce7d.firebaseapp.com",
+  projectId: "webcarros-6ce7d",
+  storageBucket: "webcarros-6ce7d.firebasestorage.app",
+  messagingSenderId: "15087624574",
+  appId: "1:15087624574:web:44f488b2b065d507ea532f",
+  measurementId: "G-F4F0QXFETV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export {db, auth, storage};
