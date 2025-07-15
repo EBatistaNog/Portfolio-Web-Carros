35b87a3aa47827797c9181f27e2b0ac4f6e68430
Eduardo Batista <pro.eduardobatistanogueira@gmail.com>
2025-07-15 10:39:15 -0300

    feat: integrate Swiper for enhanced image display functionality

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,12 @@ import './index.css'
 
 import { RouterProvider } from 'react-router-dom'
 import AuthProvider from './contexts/AuthContext'
+ 
+import {register} from 'swiper/element/bundle'
+register()
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 import { Toaster } from 'react-hot-toast'
 
