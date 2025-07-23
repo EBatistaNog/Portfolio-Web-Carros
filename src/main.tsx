9535d6211b8f8d2011fdc1bb0c8986667009de9f
Eduardo Batista <pro.eduardobatistanogueira@gmail.com>
2025-07-23 13:57:03 -0300

    feat: update TypeScript version and modify Swiper CSS imports

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,7 @@ import AuthProvider from './contexts/AuthContext'
  
 import {register} from 'swiper/element/bundle'
 register()
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
+import 'swiper/swiper-bundle.css';
 import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite' // Adicionar essa linha
 
 // https://vite.dev/config/
 export default defineConfig({
+ 
   plugins: [
     tailwindcss(), // Adicionar essa linha
     react()
